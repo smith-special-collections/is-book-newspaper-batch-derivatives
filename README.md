c2814978d2c434b2e3c5f281af9228b532b272bc
Tristan Chambers <tchambers@smith.edu>
2018-06-05 10:59:22 -0400

    Actually move the paths to a separate file with just those values

---
+++ README.md
@@ -0,0 +1,15 @@
+Description
+-----------
+
+Use cases
+---------
+
+Setting up paths
+----------------
+For most systems you will need to set the path to Kakadu and Fits. Copy config.py-example to config.py and edit as needed.
+
+Tuning concurrent processes
+---------------------------
+
+Usage
+-----
---
+++ env_setup.py
@@ -0,0 +1,10 @@
+import os
+import config
+
+def setupEnvironment():
+    os.environ["PATH"] = os.environ["PATH"] + ":" + config.KAKADU_PATH
+    try:
+        LD_LIBRARY_PATH = os.environ["LD_LIBRARY_PATH"]
+    except:
+        LD_LIBRARY_PATH = ""
+    os.environ["LD_LIBRARY_PATH"] = LD_LIBRARY_PATH + config.KAKADU_PATH

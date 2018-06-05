5110d2cd9da9894493fb5165108efd2a252d109e
Tristan Chambers <tchambers@smith.edu>
2018-06-05 09:44:32 -0400

    Replacing imagemagick with kakadu for generating JP2s A All others commented out for now

--- generate-derivatives.py
+++ generate-derivatives.py
@@ -2,10 +2,11 @@
 [x] HOCR tesseract OCR format
 [x] OCR plain text
 [ ] Aggregated ocr at issue level
-[x] JP2 whatever dimensions
+[ ] Kakadu JP2
 [x] TN 256px max
 [x] JPG 767px max
 [x] LARGE_JPG 1920px max
+[ ] FITS/TECHMD
 ...
 Sample page object: https://compass-dev.fivecolleges.edu/islandora/object/test:203/manage/datastreams
 
@@ -13,6 +14,14 @@ Naively searches for any instances of OBJ.xxx under the given path and places
 derivatives next to them as siblings.
 
 Searches for any file matching OBJ.*. Assumes that it's an image.
+
+Kakadu
+------
+JP2s are generated using Kakadu, which can be downloaded here:
+http://kakadusoftware.com/downloads/
+
+Make sure it's in your path by typig ```kdu_compress``` in a terminal.
+
 """
 import os
 import runbatchprocess
@@ -20,6 +29,10 @@ import logging
 import argparse
 from datetime import datetime
 start=datetime.now()
+
+KAKADU_ARGUMENTS = '-rate 0.5 Clayers=1 Clevels=7 "Cprecincts={256,256},{256,256},{256,256},{128,128},{128,128},{64,64},{64,64},{32,32},{16,16}" "Corder=RPCL" "ORGgen_plt=yes" "ORGtparts=R" "Cblk={32,32}" Cuse_sop=yes'
+# Using Islandora default arguments for Kakadu
+# c.f.: https://github.com/Islandora/islandora_solution_pack_large_image/blob/7.x-release/includes/derivatives.inc#L199
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument("TOPFOLDER")
@@ -39,19 +52,23 @@ logging.info('Processing folder ' + TOPFOLDER)
 # (sidestep output buffering issues with very long lists of files)
 os.system("find '%s' -name 'OBJ.*' > %s" % (TOPFOLDER, FILE_LIST_FILENAME))
 
-# Generate TN.jpg derivatives
-logging.info('Generating TN.jpg derivatives')
-runbatchprocess.process(FILE_LIST_FILENAME, 'convert -resize 256x256 "$objFileName" "$objDirName/TN.jpg"', concurrentProcesses=39)
+# # Generate TN.jpg derivatives
+# logging.info('Generating TN.jpg derivatives')
+# runbatchprocess.process(FILE_LIST_FILENAME, 'convert -resize 256x256 "$objFileName" "$objDirName/TN.jpg"', concurrentProcesses=39)
 # Generate JP2.jp2 derivatives
 logging.info('Generating JP2.jp2 derivatives')
-runbatchprocess.process(FILE_LIST_FILENAME, 'convert "$objFileName" "$objDirName/JP2.jp2"', concurrentProcesses=39)
-# Generate JPG.jpg derivatives (preview jpg)
-logging.info('Generating JPG.jpg derivatives (preview jpg)')
-runbatchprocess.process(FILE_LIST_FILENAME, 'convert -resize 767x767 "$objFileName" "$objDirName/JPG.jpg"', concurrentProcesses=39)
-#logging.info('Generating LARGE_JPG.jpg derivatives')
-runbatchprocess.process(FILE_LIST_FILENAME, 'convert -resize 1920x1920 "$objFileName" "$objDirName/LARGE_JPG.jpg"', concurrentProcesses=39)
-logging.info('Generating HOCR and OCR')
-# Run tesseract, then move the output files to their proper locations
-runbatchprocess.process(FILE_LIST_FILENAME, 'tesseract "$objFileName" "$objDirName/tesseract-output" hocr txt &> "$objDirName/.tesseract-`date +%s`.log" && mv "$objDirName/tesseract-output.hocr" "$objDirName/HOCR.html" && mv "$objDirName/tesseract-output.txt" "$objDirName/OCR.txt"', concurrentProcesses=20)
+# First make tiffs uncompress so that the demonstration version of Kakadu can parse them
+runbatchprocess.process(FILE_LIST_FILENAME, 'convert -compress none "$objFileName" "$objDirName/.uncompressedOBJ.tif"', concurrentProcesses=39)
+# Then run Kakadu using the Islandora arguments
+# Kakadu is multithreaded so we set concurrentProcesses to 1
+runbatchprocess.process(FILE_LIST_FILENAME, 'kdu_compress -i "$objDirName/.uncompressedOBJ.tif" -o "$objDirName/JP2.jp2" %s &> "$objDirName/.kakadu-`date +%%s`.log"' % KAKADU_ARGUMENTS, concurrentProcesses=1)
+# # Generate JPG.jpg derivatives (preview jpg)
+# logging.info('Generating JPG.jpg derivatives (preview jpg)')
+# runbatchprocess.process(FILE_LIST_FILENAME, 'convert -resize 767x767 "$objFileName" "$objDirName/JPG.jpg"', concurrentProcesses=39)
+# #logging.info('Generating LARGE_JPG.jpg derivatives')
+# runbatchprocess.process(FILE_LIST_FILENAME, 'convert -resize 1920x1920 "$objFileName" "$objDirName/LARGE_JPG.jpg"', concurrentProcesses=39)
+# logging.info('Generating HOCR and OCR')
+# # Run tesseract, then move the output files to their proper locations
+# runbatchprocess.process(FILE_LIST_FILENAME, 'tesseract "$objFileName" "$objDirName/tesseract-output" hocr txt &> "$objDirName/.tesseract-`date +%s`.log" && mv "$objDirName/tesseract-output.hocr" "$objDirName/HOCR.html" && mv "$objDirName/tesseract-output.txt" "$objDirName/OCR.txt"', concurrentProcesses=20)
 
 logging.info('Total running time: ' + str(datetime.now()-start))

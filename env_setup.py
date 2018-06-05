9eaae55767ddcabaf1871d5a1e72ad6010ba9589
Tristan Chambers <tchambers@smith.edu>
2018-06-05 12:02:53 -0400

    Setting up fits/techmd

--- env_setup.py
+++ env_setup.py
@@ -2,7 +2,7 @@ import os
 import config
 
 def setupEnvironment():
-    os.environ["PATH"] = os.environ["PATH"] + ":" + config.KAKADU_PATH
+    os.environ["PATH"] = os.environ["PATH"] + ":" + config.KAKADU_PATH + ":" + config.FITS_PATH
     try:
         LD_LIBRARY_PATH = os.environ["LD_LIBRARY_PATH"]
     except:

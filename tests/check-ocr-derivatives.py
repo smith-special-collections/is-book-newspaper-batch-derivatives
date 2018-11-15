2a8969e7aaa97a8ce4741786e3be4f67d2496e47
TristanSmithlib <tchambers@smith.edu>
2018-11-15 14:50:21 -0500

    Use dict object to keeping track of state (rather than global ints)

--- tests/check-ocr-derivatives.py
+++ tests/check-ocr-derivatives.py
@@ -11,9 +11,6 @@ from datasets import commonEnglishWordS
 OCR_FILENAME = 'OCR.txt'
 HOCR_FILENAME = 'HOCR.html'
 
-PAGES_CHECKED = 0
-TEXTLESS_PAGES = 0
-
 def fileContains(filepath, mystring):
     """Check if a file contains a string
     """
@@ -33,20 +30,19 @@ def fileContainsCommonEnglishWords(filepath):
     # no matches... return False
     return False
 
-def checkOCR(dirname):
+def checkOCR(dirname, state):
     # Is there an OCR.txt file?
     # Does the file contain an english word? e.g. 'the, and'
-    global TEXTLESS_PAGES
     filename = dirname + OCR_FILENAME
     try:
         if fileContainsCommonEnglishWords(filename) is False:
             logging.warning("Page OCR output doesn't contain any common English words \"%s\"" % filename)
-            TEXTLESS_PAGES = TEXTLESS_PAGES + 1
+            state['TEXTLESS_PAGES'] = state['TEXTLESS_PAGES'] + 1
     except FileNotFoundError:
         logging.error('File not found %s' % filename)
-        TEXTLESS_PAGES = TEXTLESS_PAGES + 1
+        state['TEXTLESS_PAGES'] = state['TEXTLESS_PAGES'] + 1
 
-def checkHOCR(dirname):
+def checkHOCR(dirname, state):
     # Is there an HOCR.html file?
     # Does the HOCR file contain xml/html?
     filename = dirname + HOCR_FILENAME
@@ -55,6 +51,11 @@ def checkHOCR(dirname):
             logging.error("Page HOCR output doesn't contain 'html' \"%s\"" % filename)
     except FileNotFoundError:
         logging.error('File not found %s' % filename)
+
+def doCheck(dirname, state):
+    checkOCR(dirname, state)
+    checkHOCR(dirname, state)
+    state['PAGES_CHECKED'] = state['PAGES_CHECKED'] + 1
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument("TOPFOLDER")
@@ -71,15 +72,19 @@ logging.info('Checking OCR in folder ' + TOPFOLDER)
 # Go in each folder (page)
 dirnameS = glob(TOPFOLDER + "/*/")
 
+state = {
+    'PAGES_CHECKED': 0,
+    'TEXTLESS_PAGES': 0
+    }
+
 for dirname in dirnameS:
-    checkOCR(dirname)
-    checkHOCR(dirname)
-    PAGES_CHECKED = PAGES_CHECKED + 1
+    doCheck(dirname, state)
 
-logging.info('Checked %s pages', PAGES_CHECKED)
-logging.info('Pages missing OCR text: %s', TEXTLESS_PAGES)
 
-textlessRatio = TEXTLESS_PAGES/(PAGES_CHECKED/100)
+logging.info('Checked %s pages', state['PAGES_CHECKED'])
+logging.info('Pages missing OCR text: %s', state['TEXTLESS_PAGES'])
+
+textlessRatio = state['TEXTLESS_PAGES']/(state['PAGES_CHECKED']/100)
 
 if textlessRatio > 10:
     logging.error('Over 10 percent of pages missing OCR text: %s percent' % textlessRatio)

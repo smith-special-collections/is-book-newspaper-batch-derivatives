84848c12a31e5bcf62cdd073560719a58675e6c2
TristanSmithlib <tchambers@smith.edu>
2019-01-07 12:00:48 -0500

    Switch number of concurrent processes to a ratio of MAX_CPUS

--- generate-derivatives.py
+++ generate-derivatives.py
@@ -30,6 +30,7 @@ import logging
 import argparse
 from datetime import datetime
 from env_setup import setupEnvironment
+import math
 
 # Execute the environment setup function from env_setup.py
 setupEnvironment()
@@ -47,6 +48,7 @@ argparser = argparse.ArgumentParser()
 argparser.add_argument("TOPFOLDER")
 args = argparser.parse_args()
 
+MAX_CPUS = 3
 TOPFOLDER = args.TOPFOLDER
 FILE_LIST_FILENAME = '.tmpfilelist'
 
@@ -62,33 +64,33 @@ logging.info('Processing folder ' + TOPFOLDER)
 os.system("find '%s' -name 'OBJ.*' > %s" % (TOPFOLDER, FILE_LIST_FILENAME))
 
 logging.info('Generating TN.jpg derivatives')
-runbatchprocess.process(FILE_LIST_FILENAME, 'convert -resize 256x256 "$objFileName" "$objDirName/TN.jpg"', concurrentProcesses=38)
+runbatchprocess.process(FILE_LIST_FILENAME, 'convert -resize 256x256 "$objFileName" "$objDirName/TN.jpg"', concurrentProcesses=MAX_CPUS)
 logging.info('Copy representative thumbnail')
 os.system("cp %s/00001/TN.jpg %s/" % (TOPFOLDER, TOPFOLDER))
 logging.info('Generating JP2.jp2 derivatives')
 # Kakadu doesn't like 1bit tiffs. For some reason imagemagick ignores -depth 8 when going from tif to tif so we'll use png as
 # an intermediary
-runbatchprocess.process(FILE_LIST_FILENAME, 'convert -compress none "$objFileName" -depth 8 "$objDirName/.8bitOBJ.png"', concurrentProcesses=38)
-runbatchprocess.process(FILE_LIST_FILENAME, 'convert -compress none "$objDirName/.8bitOBJ.png" -depth 8 "$objDirName/.uncompressedOBJ.tif"', concurrentProcesses=38)
+runbatchprocess.process(FILE_LIST_FILENAME, 'convert -compress none "$objFileName" -depth 8 "$objDirName/.8bitOBJ.png"', concurrentProcesses=MAX_CPUS)
+runbatchprocess.process(FILE_LIST_FILENAME, 'convert -compress none "$objDirName/.8bitOBJ.png" -depth 8 "$objDirName/.uncompressedOBJ.tif"', concurrentProcesses=MAX_CPUS)
 # Then run Kakadu using the Islandora arguments
 # Kakadu is multithreaded so I expected to set concurrentProcesses to 1. However I was seeing underutilization so I set Kakadu to be not multithreaded (above) and set the concurrentProcesses to a level to 39.
-runbatchprocess.process(FILE_LIST_FILENAME, 'kdu_compress -i "$objDirName/.uncompressedOBJ.tif" -o "$objDirName/JP2.jp2" %s >> "$objDirName/.kakadu-`date +%%s`.log" 2>&1' % KAKADU_ARGUMENTS, concurrentProcesses=38)
+runbatchprocess.process(FILE_LIST_FILENAME, 'kdu_compress -i "$objDirName/.uncompressedOBJ.tif" -o "$objDirName/JP2.jp2" %s >> "$objDirName/.kakadu-`date +%%s`.log" 2>&1' % KAKADU_ARGUMENTS, concurrentProcesses=MAX_CPUS)
 logging.info('Generating JPG.jpg derivatives (preview jpg)')
-runbatchprocess.process(FILE_LIST_FILENAME, 'convert -resize 767x767 "$objFileName" "$objDirName/JPG.jpg"', concurrentProcesses=38)
+runbatchprocess.process(FILE_LIST_FILENAME, 'convert -resize 767x767 "$objFileName" "$objDirName/JPG.jpg"', concurrentProcesses=MAX_CPUS)
 logging.info('Generating LARGE_JPG.jpg derivatives')
-runbatchprocess.process(FILE_LIST_FILENAME, 'convert -resize 1920x1920 "$objFileName" "$objDirName/LARGE_JPG.jpg"', concurrentProcesses=38)
+runbatchprocess.process(FILE_LIST_FILENAME, 'convert -resize 1920x1920 "$objFileName" "$objDirName/LARGE_JPG.jpg"', concurrentProcesses=MAX_CPUS)
 logging.info('Generating HOCR and OCR')
 # Processess file for tesseract to make them easier to read
-runbatchprocess.process(FILE_LIST_FILENAME, 'convert -compress none -blur 0x2 -threshold 50% "$objDirName/.8bitOBJ.png" "$objDirName/.OCRpreprocessed.tif"', concurrentProcesses=38)
+runbatchprocess.process(FILE_LIST_FILENAME, 'convert -compress none -blur 0x2 -threshold 50% "$objDirName/.8bitOBJ.png" "$objDirName/.OCRpreprocessed.tif"', concurrentProcesses=MAX_CPUS)
 # Run tesseract, then move the output files to their proper locations
-runbatchprocess.process(FILE_LIST_FILENAME, 'tesseract "$objDirName/.OCRpreprocessed.tif" "$objDirName/tesseract-output" hocr txt >> "$objDirName/.tesseract-`date +%s`.log" 2>&1 && mv "$objDirName/tesseract-output.hocr" "$objDirName/HOCR.html" && mv "$objDirName/tesseract-output.txt" "$objDirName/OCR.txt" 2>&1', concurrentProcesses=19)
+runbatchprocess.process(FILE_LIST_FILENAME, 'tesseract "$objDirName/.OCRpreprocessed.tif" "$objDirName/tesseract-output" hocr txt >> "$objDirName/.tesseract-`date +%s`.log" 2>&1 && mv "$objDirName/tesseract-output.hocr" "$objDirName/HOCR.html" && mv "$objDirName/tesseract-output.txt" "$objDirName/OCR.txt" 2>&1', concurrentProcesses=MAX_CPUS/2)
 # Strip out DOCTYPE tag with DTD declaration so that Islandora doesn't contact w3.org for every page on index
-runbatchprocess.process(FILE_LIST_FILENAME, 'sed -i "/DOCTYPE/d" "$objDirName/HOCR.html" && sed -i "/w3\.org\/TR\/xhtml1\/DTD/d" "$objDirName/HOCR.html"', concurrentProcesses=38)
+runbatchprocess.process(FILE_LIST_FILENAME, 'sed -i "/DOCTYPE/d" "$objDirName/HOCR.html" && sed -i "/w3\.org\/TR\/xhtml1\/DTD/d" "$objDirName/HOCR.html"', concurrentProcesses=MAX_CPUS)
 # Aggregate OCR to top level object
 logging.info('Aggregating OCR')
 os.system("cat %s/*/OCR.txt > %s/OCR.txt" % (TOPFOLDER, TOPFOLDER))
 logging.info('Generating TECHMD.xml files with Fits')
-runbatchprocess.process(FILE_LIST_FILENAME, 'fits.sh -i "$objFileName" -o "$objDirName/TECHMD.xml" >>"$objDirName/.fits-`date +%s`.log" 2>&1', concurrentProcesses=19)
+runbatchprocess.process(FILE_LIST_FILENAME, 'fits.sh -i "$objFileName" -o "$objDirName/TECHMD.xml" >>"$objDirName/.fits-`date +%s`.log" 2>&1', concurrentProcesses=MAX_CPUS/2)
 # Cleanup
 logging.info('Cleaning up temporary files')
 runbatchprocess.process(FILE_LIST_FILENAME, 'rm "$objDirName/.8bitOBJ.png" && rm "$objDirName/.uncompressedOBJ.tif" && rm "$objDirName/.OCRpreprocessed.tif"', concurrentProcesses=1)
